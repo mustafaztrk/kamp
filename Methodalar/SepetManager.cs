daea7fbc1ea6fbec1c2e84fa2481ec1c429ff208
Mustafa <mustafaozturk0671@gmail.com>
2021-01-24 22:18:45 +0300

    Proje dosyası ekle.

---
+++ Methodalar/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methodalar
+{
+    class SepetManager//maneger operasyon 
+    {
+        //naming convention
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi: "+urun.Adi);
+            
+        }
+       
+
+    }
+}
